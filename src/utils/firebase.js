ac50b8914d5c4dfb2035f5b37066a45f9f70d2e0
Albertosnp <63852519+Albertosnp@users.noreply.github.com>
2021-08-21 18:03:09 +0200

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,8 @@
 import firebase from 'firebase/app';
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration here
 const firebaseConfig = {
-    apiKey: "AIzaSyCWolFiWx9UQ_hZjj3U2r1oc5x0heFVoI8",
-    authDomain: "up2volume-4cbef.firebaseapp.com",
-    projectId: "up2volume-4cbef",
-    storageBucket: "up2volume-4cbef.appspot.com",
-    messagingSenderId: "430745477952",
-    appId: "1:430745477952:web:762ccae468a4f04c0690a5"
+    
 };
 
 // Initialize Firebase
